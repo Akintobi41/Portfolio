b9664dd568ab6841e52750c484a39f54406b9441
Akintobi41 <akintobi41moyinoluwa@gmail.com>
2022-07-06 17:18:13 +0100

    updated files

---
+++ contact/contact.js
@@ -0,0 +1,18 @@
+let menuToggle = document.querySelector('.hamburger-menu');
+let hamburger = document.querySelector('.menu-toggle');
+let navigation = document.querySelector('.navigation');
+ 
+
+menuToggle.addEventListener('click', function (e) {
+    hamburger.classList.toggle('active');
+    navigation.classList.toggle('active');
+});
+
+document.body.addEventListener('click',closeMenu)
+
+function closeMenu(e){
+    if(e.target !== hamburger && e.target !== menuToggle){ 
+        navigation.classList.remove('active');
+        hamburger.classList.remove('active');
+    } 
+}
